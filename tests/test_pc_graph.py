50ee76627c4536dedc7da95f38ab32be189dbe0d
this-is-sofia <94297994+this-is-sofia@users.noreply.github.com>
2023-10-13 14:21:49 +0200

    remove tests and prints

--- tests/test_pc_graph.py
+++ tests/test_pc_graph.py
@@ -8,30 +8,6 @@ from utils import sum_lists
 
 
 class PCTestTestCase(unittest.TestCase):
-    def test_full_graph(self):
-        test_data = []
-
-        n = 100000
-        sample_size = 5
-
-        samples = {}
-
-        for i in range(sample_size):
-            x = [random.normalvariate(mu=0.0, sigma=1.0) for _ in range(n)]
-            noise_y = [random.normalvariate(mu=0.0, sigma=1.0) for _ in range(n)]
-            samples[str(i)] = sum_lists([5 * x_val for x_val in x], noise_y)
-
-        for i in range(n):
-            entry = {}
-            for key in samples.keys():
-                entry[key] = samples[key][i]
-            test_data.append(entry)
-
-        tst = PCGraph()
-        tst.create_graph_from_data(test_data)
-        tst.create_all_possible_edges()
-        tst.execute_pipeline_steps()
-        show_edges(tst.graph)
 
     def test_with_rki_data(self):
         with open("./tests/fixtures/rki-data.csv") as f:
