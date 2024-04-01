fix(causal-effect): calculate direct-effect correctly
- simple direct effct calculation (still relatively slow)

Co-authored-by: this-is-sofia <faltenbacher.sofia@gmail.com>

causy/causal_effect_estimation/multivariate_regression.py
@@ -6,7 +6,6 @@ from causy.generators import PairsWithEdgesInBetweenGenerator
 from causy.interfaces import (
     BaseGraphInterface,
     TestResult,
-    ComparisonSettings,
     PipelineStepInterface,
     TestResultAction,
 )
@@ -23,6 +22,10 @@ class ComputeDirectEffectsMultivariateRegression(PipelineStepInterface):
         """
         effect_variable = graph.nodes[nodes[1]]
         cause_variable = graph.nodes[nodes[0]]
+
+        if graph.undirected_edge_exists(cause_variable, effect_variable):
+            # if the edge is not undirected, we do not need to calculate the direct effect
+            return
 
         edge_data = graph.edge_value(cause_variable, effect_variable)
 
@@ -41,18 +44,15 @@ class ComputeDirectEffectsMultivariateRegression(PipelineStepInterface):
             ],
             dim=1,
         )
-        print(effect_variable_parents_values.shape)
         # multivariate regression via tridiagonal reduction and SVD, see torch documentation
-        # TODO: We compute all edge weights that have ingoing edges to variables with several parents multiple times (once for each parent)
-        #       We should cache the results of the regression for each variable with multiple parents
         reshaped_effect_variable_values = effect_variable.values.view(-1, 1)
-        print(reshaped_effect_variable_values.shape)
+
         coefficients = torch.linalg.lstsq(
-            reshaped_effect_variable_values,
             effect_variable_parents_values,
+            reshaped_effect_variable_values,
             driver="gelsd",
         ).solution
-        print(coefficients)
+
         edge_data["direct_effect"] = coefficients[0][0].item()
 
         return TestResult(

tests/test_pc_graph.py
@@ -26,9 +26,9 @@ def generate_data_minimal_example(a, b, c, d, sample_size):
     return test_data
 
 
-def generate_data_further_example(a, b, c, d, e, f, g, sample_size):
-    A = a * np.random.normal(0, 1, sample_size)
-    B = b * np.random.normal(0, 1, sample_size)
+def generate_data_further_example(c, d, e, f, g, sample_size):
+    A = np.random.normal(0, 1, sample_size)
+    B = np.random.normal(0, 1, sample_size)
     C = A + c * B + np.random.normal(0, 1, sample_size)
     D = d * A + B + C + np.random.normal(0, 1, sample_size)
     E = e * B + np.random.normal(0, 1, sample_size)
@@ -75,12 +75,44 @@ class PCTestTestCase(unittest.TestCase):
         retrieve_edges(tst.graph)
         node_mapping = {}
 
+        """
         for e in retrieve_edges(tst.graph):
             print(tst.graph.nodes[e[0]].name + " => " + tst.graph.nodes[e[1]].name)
             print(tst.graph.edges[e[0]][e[1]].metadata)
+        """
 
         for key, node in tst.graph.nodes.items():
             node_mapping[node.name] = key
+
+        # check the direct_effect values
+        self.assertAlmostEqual(
+            tst.graph.edges[node_mapping["Z"]][node_mapping["Y"]].metadata[
+                "direct_effect"
+            ],
+            6.0,
+            1,
+        )
+        self.assertAlmostEqual(
+            tst.graph.edges[node_mapping["Z"]][node_mapping["X"]].metadata[
+                "direct_effect"
+            ],
+            5.0,
+            1,
+        )
+        self.assertAlmostEqual(
+            tst.graph.edges[node_mapping["V"]][node_mapping["Z"]].metadata[
+                "direct_effect"
+            ],
+            1.0,
+            1,
+        )
+        self.assertAlmostEqual(
+            tst.graph.edges[node_mapping["W"]][node_mapping["Z"]].metadata[
+                "direct_effect"
+            ],
+            1.0,
+            1,
+        )
 
         self.assertTrue(
             tst.graph.only_directed_edge_exists(
@@ -104,8 +136,8 @@ class PCTestTestCase(unittest.TestCase):
         )
 
     def test_with_larger_toy_model(self):
-        a, b, c, d, e, f, g, sample_size = 1.2, 1.7, 2, 1.5, 3, 4, 1.8, 10000
-        test_data = generate_data_further_example(a, b, c, d, e, f, g, sample_size)
+        c, d, e, f, g, sample_size = 2, 3, 4, 5, 6, 10000
+        test_data = generate_data_further_example(c, d, e, f, g, sample_size)
         tst = PC()
         node_mapping = {}
 
@@ -113,9 +145,11 @@ class PCTestTestCase(unittest.TestCase):
         tst.create_all_possible_edges()
         tst.execute_pipeline_steps()
 
+        """
         for e in retrieve_edges(tst.graph):
             print(tst.graph.nodes[e[0]].name + " => " + tst.graph.nodes[e[1]].name)
             print(tst.graph.edges[e[0]][e[1]].metadata)
+        """
 
         for key, node in tst.graph.nodes.items():
             node_mapping[node.name] = key
@@ -125,6 +159,13 @@ class PCTestTestCase(unittest.TestCase):
                 tst.graph.nodes[node_mapping["A"]], tst.graph.nodes[node_mapping["B"]]
             )
         )
+
+        self.assertFalse(
+            tst.graph.edge_exists(
+                tst.graph.nodes[node_mapping["B"]], tst.graph.nodes[node_mapping["A"]]
+            )
+        )
+
         self.assertTrue(
             tst.graph.directed_edge_exists(
                 tst.graph.nodes[node_mapping["A"]], tst.graph.nodes[node_mapping["C"]]
@@ -183,6 +224,18 @@ class PCTestTestCase(unittest.TestCase):
         self.assertFalse(
             tst.graph.edge_exists(
                 tst.graph.nodes[node_mapping["A"]], tst.graph.nodes[node_mapping["F"]]
+            )
+        )
+
+        self.assertFalse(
+            tst.graph.edge_exists(
+                tst.graph.nodes[node_mapping["E"]], tst.graph.nodes[node_mapping["D"]]
+            )
+        )
+
+        self.assertFalse(
+            tst.graph.edge_exists(
+                tst.graph.nodes[node_mapping["E"]], tst.graph.nodes[node_mapping["C"]]
             )
         )
 