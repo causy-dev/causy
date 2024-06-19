1cc33ac00142578cf6b932cadc8f73531f020175
Sofia Faltenbacher <faltenbacher.sofia@gmail.com>
2024-06-19 16:09:38 +0200

    added snapshot tests for rki example

--- tests/test_pc_e2e.py
+++ tests/test_pc_e2e.py
@@ -1,3 +1,5 @@
+import csv
+
 from causy.causal_discovery.constraint.algorithms.pc import PC_EDGE_TYPES
 from causy.common_pipeline_steps.calculation import CalculatePearsonCorrelations
 from causy.graph_model import graph_model_factory
@@ -10,13 +12,13 @@ from causy.models import Algorithm
 from causy.causal_discovery.constraint.orientation_rules.pc import ColliderTest
 from causy.sample_generator import IIDSampleGenerator, SampleEdge, NodeReference
 
-from tests.utils import CausyTestCase, load_fixture_graph
+from tests.utils import CausyTestCase, load_fixture_graph, dump_fixture_graph
 
 
 class PCTestTestCase(CausyTestCase):
     SEED = 1
 
-    def _sample_generator(self):
+    def _sample_generator_simulated_data(self):
         rdnv = self.seeded_random.normalvariate
         return IIDSampleGenerator(
             edges=[
@@ -43,7 +45,7 @@ class PCTestTestCase(CausyTestCase):
                 name="PC",
             )
         )
-        sample_generator = self._sample_generator()
+        sample_generator = self._sample_generator_simulated_data()
         test_data, graph = sample_generator.generate(10000)
         tst = algo()
         tst.create_graph_from_data(test_data)
@@ -68,7 +70,7 @@ class PCTestTestCase(CausyTestCase):
                 name="PC",
             )
         )
-        sample_generator = self._sample_generator()
+        sample_generator = self._sample_generator_simulated_data()
         test_data, graph = sample_generator.generate(10000)
         tst = algo()
         tst.create_graph_from_data(test_data)
@@ -94,7 +96,7 @@ class PCTestTestCase(CausyTestCase):
                 name="PC",
             )
         )
-        sample_generator = self._sample_generator()
+        sample_generator = self._sample_generator_simulated_data()
         test_data, graph = sample_generator.generate(10000)
         tst = algo()
         tst.create_graph_from_data(test_data)
@@ -121,7 +123,7 @@ class PCTestTestCase(CausyTestCase):
                 name="PC",
             )
         )
-        sample_generator = self._sample_generator()
+        sample_generator = self._sample_generator_simulated_data()
         test_data, graph = sample_generator.generate(10000)
         tst = algo()
         tst.create_graph_from_data(test_data)
@@ -149,7 +151,7 @@ class PCTestTestCase(CausyTestCase):
                 name="PC",
             )
         )
-        sample_generator = self._sample_generator()
+        sample_generator = self._sample_generator_simulated_data()
         test_data, graph = sample_generator.generate(10000)
         tst = algo()
         tst.create_graph_from_data(test_data)
@@ -159,3 +161,165 @@ class PCTestTestCase(CausyTestCase):
         reference = load_fixture_graph("tests/fixtures/pc_e2e/pc_collider_test.json")
         # dump_fixture_graph(tst.graph, "fixtures/pc_e2e/pc_collider_test.json")
         self.assertGraphStructureIsEqual(reference, tst.graph)
+
+    def test_pc_rki_calculate_pearson_correlations(self):
+        """
+        Test conditional independence of ordered pairs given pairs of other variables works.
+        """
+        algo = graph_model_factory(
+            Algorithm(
+                pipeline_steps=[
+                    CalculatePearsonCorrelations(),
+                ],
+                edge_types=PC_EDGE_TYPES,
+                extensions=[],
+                name="PC",
+            )
+        )
+        with open("./fixtures/rki-data.csv") as f:
+            data = csv.DictReader(f)
+            test_data_rki = []
+            for row in data:
+                for k in row.keys():
+                    if row[k] == "":
+                        row[k] = 0.0
+                    row[k] = float(row[k])
+                test_data_rki.append(row)
+        tst = algo()
+        tst.create_graph_from_data(test_data_rki)
+        tst.create_all_possible_edges()
+        tst.execute_pipeline_steps()
+        reference = load_fixture_graph(
+            "tests/fixtures/pc_e2e_rki/pc_calculate_pearson_correlations.json"
+        )
+        # dump_fixture_graph(tst.graph, "fixtures/pc_e2e_rki/pc_calculate_pearson_correlations.json")
+        self.assertGraphStructureIsEqual(reference, tst.graph)
+
+    def test_pc_rki_correlation_coefficient_test(self):
+        algo = graph_model_factory(
+            Algorithm(
+                pipeline_steps=[
+                    CalculatePearsonCorrelations(),
+                    CorrelationCoefficientTest(threshold=0.05),
+                ],
+                edge_types=PC_EDGE_TYPES,
+                extensions=[],
+                name="PC",
+            )
+        )
+        with open("./fixtures/rki-data.csv") as f:
+            data = csv.DictReader(f)
+            test_data_rki = []
+            for row in data:
+                for k in row.keys():
+                    if row[k] == "":
+                        row[k] = 0.0
+                    row[k] = float(row[k])
+                test_data_rki.append(row)
+        tst = algo()
+        tst.create_graph_from_data(test_data_rki)
+        tst.create_all_possible_edges()
+        tst.execute_pipeline_steps()
+        reference = load_fixture_graph(
+            "tests/fixtures/pc_e2e_rki/pc_correlation_coefficient_test.json"
+        )
+        # dump_fixture_graph(tst.graph, "fixtures/pc_e2e_rki/pc_correlation_coefficient_test.json")
+        self.assertGraphStructureIsEqual(reference, tst.graph)
+
+    def test_pc_rki_partial_correlation_test(self):
+        algo = graph_model_factory(
+            Algorithm(
+                pipeline_steps=[
+                    CalculatePearsonCorrelations(),
+                    CorrelationCoefficientTest(threshold=0.05),
+                    PartialCorrelationTest(threshold=0.05),
+                ],
+                edge_types=PC_EDGE_TYPES,
+                extensions=[],
+                name="PC",
+            )
+        )
+        with open("./fixtures/rki-data.csv") as f:
+            data = csv.DictReader(f)
+            test_data_rki = []
+            for row in data:
+                for k in row.keys():
+                    if row[k] == "":
+                        row[k] = 0.0
+                    row[k] = float(row[k])
+                test_data_rki.append(row)
+        tst = algo()
+        tst.create_graph_from_data(test_data_rki)
+        tst.create_all_possible_edges()
+        tst.execute_pipeline_steps()
+        reference = load_fixture_graph(
+            "tests/fixtures/pc_e2e_rki/pc_partial_correlation_test.json"
+        )
+        # dump_fixture_graph(tst.graph, "fixtures/pc_e2e_rki/pc_partial_correlation_test.json")
+        self.assertGraphStructureIsEqual(reference, tst.graph)
+
+    def test_pc_rki_extended_partial_correlation_test_matrix(self):
+        algo = graph_model_factory(
+            Algorithm(
+                pipeline_steps=[
+                    CalculatePearsonCorrelations(),
+                    CorrelationCoefficientTest(threshold=0.05),
+                    PartialCorrelationTest(threshold=0.05),
+                    ExtendedPartialCorrelationTestMatrix(threshold=0.05),
+                ],
+                edge_types=PC_EDGE_TYPES,
+                extensions=[],
+                name="PC",
+            )
+        )
+        with open("./fixtures/rki-data.csv") as f:
+            data = csv.DictReader(f)
+            test_data_rki = []
+            for row in data:
+                for k in row.keys():
+                    if row[k] == "":
+                        row[k] = 0.0
+                    row[k] = float(row[k])
+                test_data_rki.append(row)
+        tst = algo()
+        tst.create_graph_from_data(test_data_rki)
+        tst.create_all_possible_edges()
+        tst.execute_pipeline_steps()
+        reference = load_fixture_graph(
+            "tests/fixtures/pc_e2e_rki/pc_extended_partial_correlation_test_matrix.json"
+        )
+        # dump_fixture_graph(tst.graph, "fixtures/pc_e2e_rki/pc_extended_partial_correlation_test_matrix.json")
+        self.assertGraphStructureIsEqual(reference, tst.graph)
+
+    def test_pc_rki_collider_test(self):
+        algo = graph_model_factory(
+            Algorithm(
+                pipeline_steps=[
+                    CalculatePearsonCorrelations(),
+                    CorrelationCoefficientTest(threshold=0.05),
+                    PartialCorrelationTest(threshold=0.05),
+                    ExtendedPartialCorrelationTestMatrix(threshold=0.05),
+                    ColliderTest(display_name="Collider Test"),
+                ],
+                edge_types=PC_EDGE_TYPES,
+                extensions=[],
+                name="PC",
+            )
+        )
+        with open("./fixtures/rki-data.csv") as f:
+            data = csv.DictReader(f)
+            test_data_rki = []
+            for row in data:
+                for k in row.keys():
+                    if row[k] == "":
+                        row[k] = 0.0
+                    row[k] = float(row[k])
+                test_data_rki.append(row)
+        tst = algo()
+        tst.create_graph_from_data(test_data_rki)
+        tst.create_all_possible_edges()
+        tst.execute_pipeline_steps()
+
+        # reference = load_fixture_graph("tests/fixtures/pc_e2e_rki/pc_collider_test.json")
+        dump_fixture_graph(tst.graph, "fixtures/pc_e2e_rki/pc_collider_test.json")
+        # self.assertGraphStructureIsEqual(reference, tst.graph)
